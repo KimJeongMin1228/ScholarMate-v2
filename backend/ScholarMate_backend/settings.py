88148af1a8bf78c23a24fb4aba9549743e081f90
KimJeongMin1228 <jeongming353@gamil.com>
2026-05-05 12:31:45 +0900

    feat:  dockerize backend(django + nginx + redis)

--- backend/ScholarMate_backend/settings.py
+++ backend/ScholarMate_backend/settings.py
@@ -41,7 +41,7 @@ ENABLE_EMAIL_VERIFICATION = True
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.getenv("REDIS_CACHE_URL", "redis://127.0.0.1:6379/1"),
+        "LOCATION": os.getenv("REDIS_CACHE_URL", "redis://redis:6379/1"),
         "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
         "KEY_PREFIX": "scholarmate",
         "TIMEOUT": 300,
